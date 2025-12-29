ed9054ea9d8a57b4344edf2d3c77e6f3e39955a1
exitosolk <illeperumasc@gmail.com>
2025-12-29 07:59:51 +0530

    fb page link added

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -87,7 +87,7 @@ export default function Footer() {
             © {currentYear} OneCeylon. All rights reserved.
           </p>
           <div className="flex gap-6 mt-4 md:mt-0">
-            <a href="#" className="text-gray-600 hover:text-blue-600">
+            <a href="https://facebook.com/oneceylonspace" target="_blank" rel="noopener noreferrer" className="text-gray-600 hover:text-blue-600">
               <span className="sr-only">Facebook</span>
               <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
                 <path fillRule="evenodd" d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z" clipRule="evenodd" />
